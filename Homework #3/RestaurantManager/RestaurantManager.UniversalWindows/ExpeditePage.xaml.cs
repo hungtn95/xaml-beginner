7b507c50bf734a10708392297669b79b0c2f1899
Hung Nguyen <hungtn95@gmail.com>
2015-10-12 18:28:59 -0500

    Homework #3

---
+++ Homework #3/RestaurantManager/RestaurantManager.UniversalWindows/ExpeditePage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+namespace RestaurantManager.UniversalWindows
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    using RestaurantManager.Models;
+    public sealed partial class ExpeditePage : Page
+    {
+        public ExpeditePage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void AppBarButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(MainPage));
+        }
+
+        private void Button_Click1(object sender, RoutedEventArgs e)
+        {
+            DataManager t = (DataManager)ListViewMenu.DataContext;
+            if (ListViewMenu.SelectedIndex != -1) 
+                t.CurrentlySelectedMenuItems.Add((String)ListViewMenu.SelectedValue);
+        }
+
+        private void Button_Click2(object sender, RoutedEventArgs e)
+        {
+            DataManager t = (DataManager)ListViewSelected.DataContext;
+            if (t.CurrentlySelectedMenuItems.Count != 0)
+            {
+                t.OrderItems.Add(String.Join(", ", t.CurrentlySelectedMenuItems));
+                t.CurrentlySelectedMenuItems.Clear();
+            }
+        }
+    }
+}
---
+++ Homework #3/RestaurantManager/RestaurantManager.UniversalWindows/OrderPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace RestaurantManager.UniversalWindows
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    using RestaurantManager.Models;
+    public sealed partial class Order : Page
+    {
+        public Order()
+        {
+            this.InitializeComponent();
+        }
+        private void AppBarButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(MainPage));
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            DataManager t = (DataManager)GridView1.DataContext;
+            t.OrderItems.Clear();
+        }
+
+    }
+}
